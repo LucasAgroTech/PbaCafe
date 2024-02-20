acb06c7fb7bbe6dd040201e82d38c34ae3f76e1a
LucasAgroTech <lucas.negociosagro@gmail.com>
2024-02-20 16:57:18 -0300

    ultimocoomiit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_mail import Mail, Message
 from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 import cloudinary.uploader
@@ -6,27 +7,34 @@ import cloudinary
 import os
 
 app = Flask(__name__)
-uri = os.getenv(
-    "DATABASE_URL"
-)  # Obter a URI do banco de dados a partir das variáveis de ambiente
+uri = os.getenv("DATABASE_URL")
 if uri and uri.startswith("postgres://"):
-    uri = uri.replace(
-        "postgres://", "postgresql://", 1
-    )  # Substituir apenas a primeira ocorrência
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_DATABASE_URI"] = uri
 db = SQLAlchemy(app)
 
 cloudinary.config(
-    cloud_name="hbmghdcte",
-    api_key="728847166383671",
-    api_secret="PJPwG2x4O2GnsgxVmjfQg8ppJx4",
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
 )
+
+# Configurações do Flask-Mail
+app.config["MAIL_SERVER"] = "smtp.hostinger.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+mail = Mail(app)
 
 
 class Inscricao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome_desafio = db.Column(db.String(120), nullable=False)
     responsavel = db.Column(db.String(120), nullable=False)
+    email_usuario = db.Column(db.String(255), nullable=False)
     departamento = db.Column(db.String(50), nullable=False)
     area = db.Column(db.String(50), nullable=False)
     descricao = db.Column(db.Text, nullable=False)
@@ -38,7 +46,8 @@ class Inscricao(db.Model):
     recursos = db.Column(db.Text, nullable=False)
     tentativas = db.Column(db.String(120), nullable=True)
     link_materiais = db.Column(db.String(120), nullable=True)
-    anexo_url = db.Column(db.String(255), nullable=True)  # Mova esta linha para aqui
+    anexo_url = db.Column(db.String(255), nullable=True)
+    aceite_termos = db.Column(db.Boolean, default=False, nullable=False)
 
 
 def create_tables():
@@ -51,9 +60,9 @@ create_tables()
 
 @app.route("/inscricao", methods=["POST"])
 def add_inscricao():
-    # Captura os dados do formulário
     nome_desafio = request.form["nome_desafio"]
     responsavel = request.form["responsavel"]
+    email_usuario = request.form["email_usuario"]
     departamento = request.form["departamento"]
     area = request.form["area"]
     descricao = request.form["descricao"]
@@ -63,14 +72,14 @@ def add_inscricao():
     explicacao = request.form["explicacao"]
     valor_agregado = request.form["valor_agregado"]
     recursos = request.form["recursos"]
-    tentativas = request.form.get("tentativas")
-    link_materiais = request.form.get("link_materiais")
-    anexo_url = db.Column(db.String(255), nullable=True)
+    tentativas = request.form.get("tentativas", "")
+    link_materiais = request.form.get("link_materiais", "")
+    aceite_termos = request.form.get("aceite_termos") == "on"
 
-    # Cria uma nova instância do modelo Inscricao
     nova_inscricao = Inscricao(
         nome_desafio=nome_desafio,
         responsavel=responsavel,
+        email_usuario=email_usuario,
         departamento=departamento,
         area=area,
         descricao=descricao,
@@ -82,22 +91,35 @@ def add_inscricao():
         recursos=recursos,
         tentativas=tentativas,
         link_materiais=link_materiais,
+        aceite_termos=aceite_termos,
     )
 
-    # Verifica se há um arquivo no formulário
     file_to_upload = request.files.get("anexar_documentos")
     if file_to_upload and file_to_upload.filename != "":
-        filename = secure_filename(file_to_upload.filename)
         upload_result = cloudinary.uploader.upload(file_to_upload)
         anexo_url = upload_result["url"]
-        nova_inscricao.anexo_url = (
-            anexo_url  # Certifique-se de que esta linha está correta
-        )
+        nova_inscricao.anexo_url = anexo_url
 
     db.session.add(nova_inscricao)
     db.session.commit()
 
-    return redirect(url_for("index"))
+    # Preparando o conteúdo do e-mail utilizando um template HTML
+    to_email = email_usuario  # Usa o e-mail fornecido pelo usuário
+    subject = "Confirmação de Inscrição"
+
+    # Renderiza o template HTML como string, passando a variável 'responsavel' como 'nome_produtor'
+    html_content = render_template("email_template.html", nome_produtor=responsavel)
+
+    # Dispara o e-mail após salvar a inscrição
+    send_email(to_email, subject, html_content)
+
+    return redirect(url_for("index", success=True))
+
+
+def send_email(to_email, subject, html_content):
+    with app.app_context():
+        msg = Message(subject, recipients=[to_email], html=html_content)
+        mail.send(msg)
 
 
 @app.route("/", methods=["GET"])
